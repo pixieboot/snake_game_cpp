353e0947ae64591bbf03dc2f323a9ec4cd132d6f
pixieboot <lab_damjanovicn@protonmail.com>
2026-01-24 18:58:05 +0100

    code refactoring and added game over event

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@
 
 // Randomizes the type of fruit string (visually)
 // NOTE: might add different points system depending on the fruit type
-std::string_view getRandFruitType(const int x)
+char getRandFruitType(const int x)
 {
     switch (x)
     {
-    case 1: return "x";
-    case 2: return "&";
-    case 3: return "$";
-    case 4: return "#";
-    case 5: return "@";
-    case 6: return "%";
-    default: return "???";
+    case 1: return 'x';
+    case 2: return '&';
+    case 3: return '$';
+    case 4: return '#';
+    case 5: return '@';
+    case 6: return '%';
+    default: return '?';
     }
 }
 
@@ -33,34 +33,32 @@ std::string_view getRandFruitType(const int x)
 std::pair<int, int> getRandFruitPos(const std::pair<int, int>& area_dimensions,
                                     const std::pair<int, int>& player_position)
 {
-    const int AREA_WIDTH = area_dimensions.first;
-    const int AREA_HEIGHT = area_dimensions.second;
-    const int PLAYER_X = player_position.first;
-    const int PLAYER_Y = player_position.second;
+    const int a_width = area_dimensions.first;
+    const int a_height = area_dimensions.second;
+    const int p_x = player_position.first;
+    const int p_y = player_position.second;
 
-    int FRUIT_X = Random::get(1, (AREA_WIDTH - 1));
-    int FRUIT_Y = Random::get(1, (AREA_HEIGHT - 1));
-    if (PLAYER_X == FRUIT_X && PLAYER_Y == FRUIT_Y)
+    int f_x = Random::get(1, (a_width - 1));
+    int f_y = Random::get(1, (a_height - 1));
+    if (p_x == f_x && p_y == f_y)
     {
-        FRUIT_X = AREA_WIDTH - Random::get(1, (AREA_WIDTH - 1) / 2);
-        FRUIT_Y = AREA_HEIGHT - Random::get(1, (AREA_HEIGHT - 1) / 2);
+        f_x = a_width - Random::get(1, (a_width - 1) / 2);
+        f_y = a_height - Random::get(1, (a_height - 1) / 2);
     }
-    return std::make_pair(FRUIT_X, FRUIT_Y);
+    return std::make_pair(f_x, f_y);
 }
 
-/*
- * Spawns the player on a random location based of the area dimensions
+/* Spawns the player on a random location based of the area dimensions
  *
  * @param: area dimensions X and Y
  * @returns: random player X and Y coordinates
  */
 std::pair<int, int> initPlayerPos(const std::pair<int, int>& area_dimensions)
 {
-    const int AREA_WIDTH = area_dimensions.first;
-    const int AREA_HEIGHT = area_dimensions.second;
+    const int a_width = area_dimensions.first;
+    const int a_height = area_dimensions.second;
 
-    // return std::make_pair(Random::get(2, AREA_WIDTH - 2), Random::get(2, AREA_HEIGHT - 2));
-    return std::make_pair(1, 1);
+    return std::make_pair(Random::get(2, a_width - 2), Random::get(2, a_height - 2));
 }
 
 std::string_view player()
@@ -75,15 +73,75 @@ std::string_view player()
     return "0";
 }
 
-bool isGameOverText(const int i, const int j, const std::pair<int, int>& area_dimensions, const std::string_view text)
+/*  Calculates given text.size to return boolean if the given text
+ *  is within the area where it needs to be printed in the middle of the screen
+ *
+ *  @param i & j: for loop iterators
+ *  @param &area_dimensions: std::pair consisting of X and Y coordinates of area
+ *  @param std::string_view text: text that will be calculated to fit in the middle of the screen with text.size()
+ *  @param int is_game_over_text: is the text "GAME OVER", alignment purposes with if statement
+ *  @return bool: bool if the given text is in the area where it needs to be printed
+ */
+bool alignGameOverText(
+    const int i,
+    const int j,
+    const std::pair<int, int>& area_dimensions,
+    const std::string_view text,
+    const int is_game_over_text)
 {
-    const int A_WIDTH = area_dimensions.first;
-    const int A_HEIGHT = area_dimensions.second;
+    const int a_width = area_dimensions.first;
+    const int a_height = area_dimensions.second;
+    const int text_size = (a_width - static_cast<int>(text.size())) / 2;
+    int row{};
 
-    const int ROW = A_HEIGHT / 2;
-    const int COL = (A_WIDTH - static_cast<int>(text.size())) / 2;
+    // "GAME OVER" alignment
+    if (is_game_over_text == 1)
+    {
+        row = a_height / 2;
+    }
+    else // "Points: x" alignment
+    {
+        row = (a_height / 2) + 1;
+    }
 
-    return i == ROW && j >= COL && j < COL + static_cast<int>(text.size());
+    return i == row && j >= text_size && j < text_size + static_cast<int>(text.size());
+}
+
+/*  Returns chars of strings to display game over text status at the
+ *  end of the game, it calls align function to check in which area
+ *  should it start printing the text in the middle of the screen
+ *
+ *  @param i & j: for loop iterators
+ *  @param &area_dimensions: std::pair consisting of X and Y coordinates of area
+ *  @param int points: points to be printed alongside text
+ *  @return char: returns a char that needs to be printer to the called
+ */
+char showGameOverStats(
+    const int i,
+    const int j,
+    const std::pair<int, int>& area_dimensions,
+    const int points)
+{
+    const int a_width = area_dimensions.first;
+    const int a_height = area_dimensions.second;
+
+    constexpr const std::string_view game_over_text = "GAME OVER";
+    const std::string_view points_text = "Points: " + std::to_string(points); // look for a better solution later
+
+    // prints "GAME OVER"
+    if (alignGameOverText(i, j, area_dimensions, game_over_text, 1))
+    {
+        const int col = (a_width - static_cast<int>(game_over_text.size())) / 2;
+        return game_over_text[j - col];
+    }
+
+    // prints "Points: X"
+    if (alignGameOverText(i, j, area_dimensions, points_text, 0))
+    {
+        const int col = (a_width - static_cast<int>(points_text.size())) / 2;
+        return points_text[j - col];
+    }
+    return ' ';
 }
 
 char renderCells(
@@ -92,27 +150,26 @@ char renderCells(
     const std::pair<int, int>& area_dimensions,
     const std::pair<int, int>& player_position,
     const std::pair<int, int>& fruit_position,
-    const std::string_view fruit_type,
-    const bool game_over_status)
+    const char fruit_type,
+    const bool game_over_status,
+    const int points)
 {
-    const int A_WIDTH = area_dimensions.first;
-    const int A_HEIGHT = area_dimensions.second;
+    const int a_width = area_dimensions.first;
+    const int a_height = area_dimensions.second;
 
-    const int P_X = player_position.first;
-    const int P_Y = player_position.second;
+    const int p_x = player_position.first;
+    const int p_y = player_position.second;
 
-    const int F_X = fruit_position.first;
-    const int F_Y = fruit_position.second;
-
-    constexpr std::string_view GAME_OVER = "GAME OVER";
+    const int f_x = fruit_position.first;
+    const int f_y = fruit_position.second;
 
     // top
-    if (i == 0 && j > 0 && j < A_WIDTH - 1)
+    if (i == 0 && j > 0 && j < a_width - 1)
     {
         return '^';
     }
     // bottom
-    if (i == A_HEIGHT - 1 && j > 0 && j < A_WIDTH - 1)
+    if (i == a_height - 1 && j > 0 && j < a_width - 1)
     {
         return 'v';
     }
@@ -122,25 +179,24 @@ char renderCells(
         return '<';
     }
     // right
-    if (j == A_WIDTH - 1)
+    if (j == a_width - 1)
     {
         return '>';
     }
     // fruit
-    if (j == F_X && i == F_Y)
+    if (j == f_x && i == f_y)
     {
-        return 'F';
+        return fruit_type;
     }
     // player
-    if (j == P_X && i == P_Y)
+    if (j == p_x && i == p_y)
     {
         return 'P';
     }
-    // check if its game over
-    if (game_over_status && isGameOverText(i, j, area_dimensions, GAME_OVER))
+    // check if its game over and print game over stats
+    if (game_over_status)
     {
-        const int col = (A_WIDTH - static_cast<int>(GAME_OVER.size())) / 2;
-        return "GAME OVER"[j - col];
+        return showGameOverStats(i, j, area_dimensions, points);
     }
     return ' ';
 }
@@ -149,11 +205,11 @@ std::string_view renderPoints(const int p)
 {
     if (p > 999)
     {
-        std::cout << "Points: 999+" << std::endl;
+        std::cout << "Points: 999+\n";
     }
     else
     {
-        std::cout << "Points: " << p << std::endl;
+        std::cout << "Points: " << p << '\n';
     }
     return "???";
 }
@@ -163,53 +219,60 @@ void renderArea(
     const std::pair<int, int>& player_position,
     const std::string_view display_player,
     const std::pair<int, int>& fruit_position,
-    const std::string_view fruit_type,
+    const char fruit_type,
     const int points,
     const bool game_over_status)
 {
-    const int AREA_WIDTH = area_dimensions.first;
-    const int AREA_HEIGHT = area_dimensions.second;
+    const int a_width = area_dimensions.first;
+    const int a_height = area_dimensions.second;
 
     // needs to be applied outside debug build as system("clear") is performance drop (shell spawn)
     // constexpr const char* CLEAR_SCREEN = "\033[2J\033[H";
     // std::cout << CLEAR_SCREEN;
 
     system("clear");
-    // const std::string SHOW_GAME_OVER{"GAME OVER"};
 
     renderPoints(points);
-    for (int i = 0; i < AREA_HEIGHT; i++)
+    for (int i = 0; i < a_height; i++)
     {
-        for (int j = 0; j < AREA_WIDTH; j++)
+        for (int j = 0; j < a_width; j++)
         {
             std::cout << renderCells(i, j, area_dimensions, player_position, fruit_position, fruit_type,
-                                     game_over_status);
+                                     game_over_status, points);
         }
-        std::cout << std::endl;
+        std::cout << '\n';
     }
 }
 
-int isFruitEaten(
+/* Checks if the fruit location is the same as player location
+ *
+ *  @param &player_position: std::pair consisting of X and Y coordinates of the player
+ *  @param &fruit_position: std::pair consisting of X and Y coordinates of the fruit spawn
+ *  @return: true or false
+ */
+bool isFruitEaten(
     const std::pair<int, int>& player_position,
-    const std::pair<int, int>& fruit_position,
-    int* points)
+    const std::pair<int, int>& fruit_position)
 {
-    const int P_X = player_position.first;
-    const int P_Y = player_position.second;
-    const int F_X = fruit_position.first;
-    const int F_Y = fruit_position.second;
+    const int p_x = player_position.first;
+    const int p_y = player_position.second;
+    const int f_x = fruit_position.first;
+    const int f_y = fruit_position.second;
 
-    if (P_X == F_X && P_Y == F_Y)
+    if (p_x == f_x && p_y == f_y)
     {
-        points++;
-        return 1;
+        return true;
     }
     else
     {
-        return 0;
+        return false;
     }
 }
 
+
+/* Sets the terminal buffer to unbuffered mode for
+ * non-blocking user input when the snake is controlled
+ */
 void setUnbufferedInput()
 {
     struct termios old_tio{}, new_tio{};
@@ -227,20 +290,26 @@ void setUnbufferedInput()
     tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
 }
 
-int checkForCollisions(const std::pair<int, int>& player_position, const std::pair<int, int>& area_dimensions)
+/*  Checks if player has reached the edges of the area walls
+ *
+ *  @param &player_position: std::pair consisting of X and Y coordinates of the player
+ *  @param &area_dimensions: std::pair consisting of X and Y dimensions for the area
+ *  @return: bool
+ */
+bool checkForCollisions(const std::pair<int, int>& player_position, const std::pair<int, int>& area_dimensions)
 {
-    const int P_X = player_position.first;
-    const int P_Y = player_position.second;
+    const int p_x = player_position.first;
+    const int p_y = player_position.second;
 
-    const int A_WIDTH = area_dimensions.first;
-    const int A_HEIGHT = area_dimensions.second;
+    const int a_width = area_dimensions.first;
+    const int a_height = area_dimensions.second;
 
-    if (P_X == 0 || P_X == A_WIDTH - 1 || P_Y == 0 || P_Y == A_HEIGHT - 1)
+    if (p_x == 0 || p_x == a_width - 1 || p_y == 0 || p_y == a_height - 1)
     {
-        return 1;
+        return true;
     }
     // else if (player_pos_x == player_pos_x) CHECK IF PLAYER EATS ITS OWN TAIL
-    return 0;
+    return false;
 }
 
 int main()
@@ -248,18 +317,18 @@ int main()
     // set terminal settings for unbuffered input
     setUnbufferedInput();
 
-    // init area dimensions 80x20
-    constexpr std::pair<int, int> AREA_DIMENSIONS = std::make_pair(80, 20);
+    // init area dimensions 80x21
+    constexpr std::pair<int, int> area_dimensions = std::make_pair(80, 21);
 
     // init random player position
-    std::pair<int, int> player_position = initPlayerPos(AREA_DIMENSIONS);
+    std::pair<int, int> player_position = initPlayerPos(area_dimensions);
 
     // show player
-    const std::string_view DISPLAY_PLAYER = player();
+    const std::string_view display_player = player();
 
     // init random fruit type and position
-    std::pair<int, int> fruit_position = getRandFruitPos(AREA_DIMENSIONS, player_position);
-    std::string_view fruit_type = getRandFruitType(Random::get(1, 6));
+    std::pair<int, int> fruit_position = getRandFruitPos(area_dimensions, player_position);
+    char fruit_type = getRandFruitType(Random::get(1, 6));
 
     // init points
     int points = 0;
@@ -268,7 +337,7 @@ int main()
     bool game_over_status = false;
 
     // init first render view
-    renderArea(AREA_DIMENSIONS, player_position, DISPLAY_PLAYER, fruit_position, fruit_type, points, game_over_status);
+    renderArea(area_dimensions, player_position, display_player, fruit_position, fruit_type, points, game_over_status);
 
     while (true)
     {
@@ -276,42 +345,65 @@ int main()
         if (c == 119) // w key
         {
             player_position.second -= 1; // player position: y-1
-            if (const int is_game_over = checkForCollisions(player_position, AREA_DIMENSIONS); is_game_over == 1)
+            if (const bool is_game_over = checkForCollisions(player_position, area_dimensions); is_game_over == true)
             {
                 game_over_status = true;
             }
-            // is_fruit_eaten(player_position, fruit_position);
-            renderArea(AREA_DIMENSIONS, player_position, DISPLAY_PLAYER, fruit_position, fruit_type, points,
+            if (const bool is_fruit_eaten = isFruitEaten(player_position, fruit_position); is_fruit_eaten == true)
+            {
+                points++;
+                fruit_type = getRandFruitType(Random::get(1, 6));
+                fruit_position = getRandFruitPos(area_dimensions, player_position);
+            }
+            renderArea(area_dimensions, player_position, display_player, fruit_position, fruit_type, points,
                        game_over_status);
         }
         else if (c == 115) // s key
         {
             player_position.second += 1; // player position: y+1
-            if (const int is_game_over = checkForCollisions(player_position, AREA_DIMENSIONS); is_game_over == 1)
+            if (const bool is_game_over = checkForCollisions(player_position, area_dimensions); is_game_over == true)
             {
                 game_over_status = true;
             }
-            renderArea(AREA_DIMENSIONS, player_position, DISPLAY_PLAYER, fruit_position, fruit_type, points,
+            if (const bool is_fruit_eaten = isFruitEaten(player_position, fruit_position); is_fruit_eaten == true)
+            {
+                points++;
+                fruit_type = getRandFruitType(Random::get(1, 6));
+                fruit_position = getRandFruitPos(area_dimensions, player_position);
+            }
+            renderArea(area_dimensions, player_position, display_player, fruit_position, fruit_type, points,
                        game_over_status);
         }
         else if (c == 97) // a key
         {
             player_position.first -= 1; // player position: x-1
-            if (const int is_game_over = checkForCollisions(player_position, AREA_DIMENSIONS); is_game_over == 1)
+            if (const bool is_game_over = checkForCollisions(player_position, area_dimensions); is_game_over == true)
             {
                 game_over_status = true;
             }
-            renderArea(AREA_DIMENSIONS, player_position, DISPLAY_PLAYER, fruit_position, fruit_type, points,
+            if (const bool is_fruit_eaten = isFruitEaten(player_position, fruit_position); is_fruit_eaten == true)
+            {
+                points++;
+                fruit_type = getRandFruitType(Random::get(1, 6));
+                fruit_position = getRandFruitPos(area_dimensions, player_position);
+            }
+            renderArea(area_dimensions, player_position, display_player, fruit_position, fruit_type, points,
                        game_over_status);
         }
         else if (c == 100) // d key
         {
             player_position.first += 1; // player position: x+1
-            if (const int is_game_over = checkForCollisions(player_position, AREA_DIMENSIONS); is_game_over == 1)
+            if (const bool is_game_over = checkForCollisions(player_position, area_dimensions); is_game_over == true)
             {
                 game_over_status = true;
             }
-            renderArea(AREA_DIMENSIONS, player_position, DISPLAY_PLAYER, fruit_position, fruit_type, points,
+            if (const bool is_fruit_eaten = isFruitEaten(player_position, fruit_position); is_fruit_eaten == true)
+            {
+                points++;
+                fruit_type = getRandFruitType(Random::get(1, 6));
+                fruit_position = getRandFruitPos(area_dimensions, player_position);
+            }
+            renderArea(area_dimensions, player_position, display_player, fruit_position, fruit_type, points,
                        game_over_status);
         }
         if (game_over_status)
